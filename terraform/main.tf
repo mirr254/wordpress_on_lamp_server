6f6bb8c90589f3b4211a6fec8f5c0cdf0570ec3d
mirr254 <sammysteppa90@gmail.com>
2019-04-08 12:20:53 +0300

    chore(tfvars) move this file to terraform directory

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
     project     = "${var.gcp_project}"
     region      = "${var.region}"
-    credentials = "${file("$var.credentials")}"
+    credentials = "${file("${var.credentials}")}"
 }
--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,5 +1,5 @@
 //we create a subnet for our vpc
-resource "google_compute_subnetwork" {
+resource "google_compute_subnetwork" "subnet" {
     name          = "${var.name}-sunetwork"
     ip_cidr_range = "${var.subnet_cidr}"
     network       = "${var.name}-vpc"
