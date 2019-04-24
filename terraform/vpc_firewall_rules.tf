047607ef6289e64834ceb469a94313bd9175849e
mirr254 <sammysteppa90@gmail.com>
2019-04-24 16:13:57 +0300

    update packer and firewall

--- terraform/vpc_firewall_rules.tf
+++ terraform/vpc_firewall_rules.tf
@@ -1,5 +1,5 @@
 resource "google_compute_firewall" "firewall" {
-    name     = "${var.name}-firewall"
+    name     = "${var.name}-allow-https-firewall"
     network  = "${google_compute_network.vpc.name}"
 
     allow {
@@ -8,19 +8,27 @@ resource "google_compute_firewall" "firewall" {
 
     allow {
         protocol = "tcp"
-        ports    = ["22", "443"]
+        ports    = ["443"]
     }
 
     source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_firewall" "allow_http" {
-    name    = "${var.name}-http-firewall"
+    name    = "${var.name}-allow-http-firewall"
     network = "${google_compute_network.vpc.name}"
 
     allow {
         protocol = "tcp"
         ports    = ["80"]
     }
+
+    source_ranges = ["0.0.0.0/0"]
+    
     target_tags = ["http"]
 }
+
+// this was not working since https traffic is not being alllowed when
+// creating the instance firewall
+
+//when allowed on console it works fine
